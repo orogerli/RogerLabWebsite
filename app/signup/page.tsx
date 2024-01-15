f126a0d63f9d78e4cba70126c0eb66251ba9033a
TrmiDo <111090515+TrmiDo@users.noreply.github.com>
2024-01-14 23:45:10 -0500

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,114 +1,46 @@
-import SectionTitle from "@/components/Common/SectionTitle2";
-const checkIcon = (
-  <svg width="16" height="13" viewBox="0 0 16 13" className="fill-current">
-    <path d="M5.8535 12.6631C5.65824 12.8584 5.34166 12.8584 5.1464 12.6631L0.678505 8.1952C0.483242 7.99994 0.483242 7.68336 0.678505 7.4881L2.32921 5.83739C2.52467 5.64193 2.84166 5.64216 3.03684 5.83791L5.14622 7.95354C5.34147 8.14936 5.65859 8.14952 5.85403 7.95388L13.3797 0.420561C13.575 0.22513 13.8917 0.225051 14.087 0.420383L15.7381 2.07143C15.9333 2.26669 15.9333 2.58327 15.7381 2.77854L5.8535 12.6631Z" />
-  </svg>
-);
+import SharePost from "@/components/Blog/SharePost";
+import TagButton from "@/components/Blog/TagButton";
+import Image from "next/image";
+
 const SignupPage = () => {
-  const List = ({ text }) => (
-    <p className="mb-5 flex items-center text-lg font-medium text-body-color" style={{ fontSize: '15px' }}>
-      <span className="mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-        {checkIcon}
-      </span>
-      {text}
-    </p>
-  );
+  
   return (
     <>
-      <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
-        <div className="w-full px-4 lg:w-1/2">
-              <SectionTitle
-                title="Amblyopia (“Lazy Eye”) Treatment Study"
-                paragraph="Children and adults with amblyopia are invited to participate in a clinical research trial conducted at the Nova Southeastern University College of Optometry. This research is set out to develop and evaluate a new vision training approach for improving lazy eye."
-                mb="44px"
-              />
-              <SectionTitle
-                title=""
-                paragraph="In the study, participants will be asked to look at a series of patterns on a monitor screen and make simple perceptual judgments. If you or your child has amblyopia and would like to participate, please contact the Principal Investigator"
-                mb="44px"
-              /> 
-      <div className="container items-center">
-          
-            
-            
+      <section className="pt-[150px] pb-[120px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            <div className="w-full px-4 lg:w-8/12">
+              <div>
+                
+                <div className="mb-10 flex flex-wrap items-center justify-between border-b border-body-color border-opacity-10 pb-4 dark:border-white dark:border-opacity-10">
+                
+                
+                <h3 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                Amblyopia (“Lazy Eye”) Treatment Study
+                  </h3>
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  Children and adults with amblyopia are invited to participate in a clinical research trial conducted at the Nova Southeastern University College of Optometry. This research is set out to develop and evaluate a new vision training approach for improving lazy eye
+                  </p>
+                  
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  In the study, participants will be asked to look at a series of patterns on a monitor screen and make simple perceptual judgments. If you or your child has amblyopia and would like to participate, please contact the Principal Investigator</p>
+                </div>
 
-            
-          
-          <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 ">
-              <SectionTitle
-                title="(2)	3D Vision Study "
-                paragraph="Adults with normal vision are invited to participate in a 3D vision study conducted at the Nova Southeastern University College of Optometry. "
-                 mb="44px"
-              />
-              <SectionTitle
-                title=""
-                paragraph="In the study, participants will be asked to look at a series of patterns on a monitor screen and make simple perceptual judgments. If you have 20/20 vision and would like to participate, please contact the Principal Investigator."
-                mb="44px"
-              /> 
+
+
+
+                <h3 className="font-xl mb-10 font-bold leading-tight text-black dark:text-white sm:text-2xl sm:leading-tight lg:text-xl lg:leading-tight xl:text-2xl xl:leading-tight">
+                3D Vision Study
+                  </h3>
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  Adults with normal vision are invited to participate in a 3D vision study conducted at the Nova Southeastern University College of Optometry.
+                  </p>
+                  
+                  <p className="mb-10 list-inside list-disc text-body-color">
+                  In the study, participants will be asked to look at a series of patterns on a monitor screen and make simple perceptual judgments. If you have 20/20 vision and would like to participate, please contact the Principal Investigator.</p>
+                  </div>
             </div>
-           
-
-            
           </div>
-        </div>
-      </div>
-        <div className="absolute left-0 top-0 z-[-1]">
-          <svg
-            width="1440"
-            height="969"
-            viewBox="0 0 1440 969"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <mask
-              id="mask0_95:1005"
-              style={{ maskType: "alpha" }}
-              maskUnits="userSpaceOnUse"
-              x="0"
-              y="0"
-              width="1440"
-              height="969"
-            >
-              <rect width="1440" height="969" fill="#090E34" />
-            </mask>
-            <g mask="url(#mask0_95:1005)">
-              <path
-                opacity="0.1"
-                d="M1086.96 297.978L632.959 554.978L935.625 535.926L1086.96 297.978Z"
-                fill="url(#paint0_linear_95:1005)"
-              />
-              <path
-                opacity="0.1"
-                d="M1324.5 755.5L1450 687V886.5L1324.5 967.5L-10 288L1324.5 755.5Z"
-                fill="url(#paint1_linear_95:1005)"
-              />
-            </g>
-            <defs>
-              <linearGradient
-                id="paint0_linear_95:1005"
-                x1="1178.4"
-                y1="151.853"
-                x2="780.959"
-                y2="453.581"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-              <linearGradient
-                id="paint1_linear_95:1005"
-                x1="160.5"
-                y1="220"
-                x2="1099.45"
-                y2="1192.04"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-            </defs>
-          </svg>
         </div>
       </section>
     </>
